8494832dbf925385f3f041f4b1054a87f9a2eff9
Maelby Muniz <maelbymuniz@gmail.com>
2026-01-02 09:40:09 -0300

    alunos: criar, editar e inativar

--- apps/alunos/urls.py
+++ apps/alunos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import listar_alunos
+from .views import criar_aluno, listar_alunos, editar_aluno, inativar_aluno
 
 urlpatterns = [
-    path('alunos/', listar_alunos, name='listar_alunos')
+    path('alunos/', listar_alunos, name='listar_alunos'),
+    path('alunos/novo/', criar_aluno, name='criar_aluno'),
+    path('alunos/<int:aluno_id>/editar/', editar_aluno, name='editar_aluno'),
+    path('alunos/<int:aluno_id>/inativar/', inativar_aluno, name='inativar_aluno')
 ]
--- apps/alunos/views.py
+++ apps/alunos/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Aluno
+from .forms import AlunoForm
+
+def criar_aluno(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_alunos')
+    else:
+        form = AlunoForm()
+        
+    return render(request, 'alunos/criar_aluno.html', {'form': form})
 
 def listar_alunos(request):
-    alunos = Aluno.objects.filter(ativo=True)
+    alunos = Aluno.objects.filter(ativo=True)  
+    return render(
+        request, 
+        'alunos/listar_alunos.html' , 
+        {'alunos': alunos})
+
+def editar_aluno(request, aluno_id):
+    aluno = get_object_or_404(Aluno, id=aluno_id)
     
-    return render(request, 'alunos/listar_alunos.html' , {'alunos': alunos})
+    if request.method == 'POST':
+        form = AlunoForm(request.POST, instance=aluno)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_alunos')
+    
+    else:
+        form = AlunoForm(instance=aluno)
+        
+    return render(
+        request,
+        'alunos/editar_aluno.html',
+        {'form': form, 'aluno': aluno}
+    )
+    
+def inativar_aluno(request, aluno_id):
+    aluno = get_object_or_404(Aluno, id=aluno_id)
+    aluno.ativo = False
+    aluno.save()
+    
+    return redirect('listar_alunos')
